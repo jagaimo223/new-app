6c09200ace21a02f0c1d709f20dd8b7d7094839d
jagaimo <rsp36563@gmail.com>
2025-02-11 13:57:42 +0900

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  root "pages#home"
-  
-  get "pages/home"
+  root 'pages#home'
+
+  resources :users, only: %i[new create]
+
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
