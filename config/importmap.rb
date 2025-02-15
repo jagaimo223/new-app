19b1bfc5b29c8bc804868ffca92e7377e7e8a7f6
jagaimo <rsp36563@gmail.com>
2025-02-15 20:39:47 +0900

    bug_fix

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
 pin "application"
-pin "header"
+
